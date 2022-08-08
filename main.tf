1e4069bf5531a4b15bf7381b31fe5b97736c7133
priyankdeshmukh <priyankdeshmukh300@gmail.com>
2022-08-08 07:53:23 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+resource "aws_instance" "test_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+ 
+
+  tags = {
+    Name = "webserver"
+
+  }
+}
+
+
+output "aws_instance_id" {
+  value = aws_instance.test_instance.id
+}
